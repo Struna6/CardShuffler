3c9ebea892ebc4043cb28449a6cd6f247fa287cd
Karol Struniawski <struniawski.karol@gmai.com>
2019-11-23 13:09:02 +0100

    initial

--- CardShuffler/ViewController.swift
+++ CardShuffler/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chosenCardLabel: UILabel!
+    @IBOutlet var cardsImages: [UIImageView]!
+    
+    
+    //TODO MAKE MODEL RESPONSIBLE FOR SHUFFLE, STORE ALL CARD TAIL, 5 CHOSEN CARDS WITH IMAGES OF THEM
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shuffleButtonPressed(_ sender: UIButton) {
+        //TODO SHUFFLE CARDS, PRESENT NEW 5 CARDS ON SCREEN
+    }
+    
+    //TODO CUSTOM IMAGEVIEWCARD SUBCLASS - WHEN CARD IS SELECTED LABEL CHANGES WHICH CARD IS SELECTED
+    
 }
 
